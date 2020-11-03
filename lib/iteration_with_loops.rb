c3d6dbdec0c5ab92ec6119ba9f2d57943bb8f10e
Winston Chen <winstonchen.wm@gmail.com>
2020-11-03 20:31:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
   row_index = 0 
   while row_index < src.length do
   element_index = 0
-  while element_index < src.length do
+  while element_index < src[row_index].length do
       if src[row_index][element_index].is_a?(String)
         new_array << src[row_index][element_index]
       end
